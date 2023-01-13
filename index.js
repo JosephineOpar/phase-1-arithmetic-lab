b71569a06d6f81b078686395200f334643d0e453
Josephine Opar <josephineopar@gmail.com>
2023-01-13 23:24:43 +0200

    commit tree 0f96e1fd817ce618d8a74c40a487dab2cfa770ab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 62
+let num2 = 1
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.ceil(Math.random() * 5)
+
+let num3 = 9
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(3, 8, -2, 3, 20)
+
